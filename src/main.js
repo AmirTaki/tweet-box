7412c5258e2b480749c6321601aaf63a41c84264
amir <amirtaki623@gmail.com>
2025-10-04 21:44:55 +0330

    part 20

--- src/main.js
+++ src/main.js
@@ -4,3 +4,8 @@ const readonlyInput = document.querySelector('.readonly')
 const placeholder = document.querySelector('.placeholder')
 const counter = document.querySelector('.counter')
 const button = document.querySelector('button')
+
+
+editableInput.onfocus = () => {
+    placeholder.style.color = '#98a5b1'
+}
